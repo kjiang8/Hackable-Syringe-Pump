2dd2e070517e6339cffa49c3b568b61cded0db44
kjiang8 <kjiang@ctemc.org>
2015-08-11 15:08:35 -0400

    added executable scripts to start nodeforwarder.js
    .bat for windows
    .sh for macs

---
+++ startnode.bat
@@ -0,0 +1,7 @@
+npm install serialport
+npm install express
+npm install sleep
+npm install socket.io
+npm install cors
+npm install body-parser
+node nodeforwarder.js 9005 COM4 9600 10000
---
+++ startnode.sh
@@ -0,0 +1,7 @@
+npm install serialport
+npm install express
+npm install sleep
+npm install socket.io
+npm install cors
+npm install body-parser
+node nodeforwarder.js 9005 /dev/tty.usb* 9600 10000
